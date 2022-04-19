f2d28b53ee4f5f61efde5dbbf394bdf0971cf57a
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-19 16:15:40 +0200

    Set up some preliminary test structure

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in activerecord-associated_object.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
+gem "sqlite3"
 
+gem "rake", "~> 13.0"
 gem "minitest", "~> 5.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "activerecord"
 require "activerecord/associated_object"
+require "logger"
 
 require "minitest/autorun"
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActiveRecord::Schema.define do
+  create_table :posts, force: true do |t|
+  end
+
+  create_table :comments, force: true do |t|
+    t.integer :post_id
+  end
+end
+
+class Post < ActiveRecord::Base
+  has_many :comments
+end
+
+class Comment < ActiveRecord::Base
+  belongs_to :post
+end
