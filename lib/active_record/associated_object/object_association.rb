3af199c3b1573e6ec2c36a92b7a4707564e5a8af
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-22 09:01:12 +0200

    Enjoy the refiner things in life
    …which is to say extract the helper into a refinement, so not every
    ActiveRecord::Base object gets the helper.

--- lib/active_record/associated_object/object_association.rb
+++ lib/active_record/associated_object/object_association.rb
@@ -1,4 +1,14 @@
 module ActiveRecord::AssociatedObject::ObjectAssociation
+  using Module.new {
+    refine Module do
+      def extend_source_from(chunks, &block)
+        location = caller_locations(1, 1).first
+        source_chunks = Array(chunks).flat_map(&block)
+        class_eval source_chunks.join("\n\n"), location.path, location.lineno
+      end
+    end
+  }
+
   def has_object(*names, **callbacks)
     extend_source_from(names) do |name|
       "def #{name}; @#{name} ||= #{self.name}::#{name.to_s.classify}.new(self); end"
@@ -9,11 +19,5 @@ module ActiveRecord::AssociatedObject::ObjectAssociation
         "#{callback} { #{name}.#{method == true ? callback : method} }"
       end
     end
-  end
-
-  def extend_source_from(chunks, &block)
-    location = caller_locations(1, 1).first
-    source_chunks = Array(chunks).flat_map(&block)
-    class_eval source_chunks.join("\n\n"), location.path, location.lineno
   end
 end
