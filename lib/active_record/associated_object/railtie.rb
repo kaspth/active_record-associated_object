a4b394bb2790154d89251d2836384669d652d877
Kasper Timm Hansen <kaspth@gmail.com>
2022-04-22 08:47:16 +0200

    We're already in Base here

--- lib/active_record/associated_object/railtie.rb
+++ lib/active_record/associated_object/railtie.rb
@@ -7,7 +7,7 @@ class ActiveRecord::AssociatedObject::Railtie < Rails::Railtie
   initializer "object_association.setup" do
     ActiveSupport.on_load :active_record do
       require "active_record/associated_object/object_association"
-      ActiveRecord::Base.extend ActiveRecord::AssociatedObject::ObjectAssociation
+      extend ActiveRecord::AssociatedObject::ObjectAssociation
     end
   end
 end
