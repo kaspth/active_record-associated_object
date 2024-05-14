d09758b0e4fefc34cfa13429e67509d32401e6df
Kasper Timm Hansen <kaspth@gmail.com>
2024-05-14 18:53:41 -0500

    Trim some of our methods we don't need now

--- lib/generators/associated/associated_generator.rb
+++ lib/generators/associated/associated_generator.rb
@@ -1,15 +1,13 @@
 class AssociatedGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
-  def generate_associated_object
-    template "associated.rb", associated_object_file
-  end
-
-  def generate_associated_object_test
-    template "associated_test.rb", associated_object_test_file
+  def generate_associated_object_files
+    template "associated.rb",      "app/models/#{name.underscore}.rb"
+    template "associated_test.rb", "test/models/#{name.underscore}_test.rb"
   end
 
   def connect_associated_object
+    record_file = "#{destination_root}/app/models/#{record_path}.rb"
     raise "Record class '#{record_klass}' does not exist" unless File.exist?(record_file)
 
     inject_into_class record_file, record_klass do
@@ -19,19 +17,11 @@ class AssociatedGenerator < Rails::Generators::NamedBase
 
   private
 
-  def record_file      = "#{destination_root}/app/models/#{record_path}.rb"
-  def record_test_file = "#{destination_root}/test/models/#{record_path}_test.rb"
-
-  def associated_object_file      = "#{destination_root}/app/models/#{record_path}/#{associated_object_path}.rb"
-  def associated_object_test_file = "#{destination_root}/test/models/#{record_path}/#{associated_object_path}_test.rb"
-
   # The `:name` argument can handle model names, but associated object class names aren't singularized.
   # So these record and associated_object methods prevent that.
-  def record_path  = record_name.downcase.underscore
-  def record_klass = record_name.camelize
-  def record_name  = name.deconstantize
+  def record_path  = record_klass.downcase.underscore
+  def record_klass = name.deconstantize
 
-  def associated_object_path  = associated_object_name.downcase.underscore
-  def associated_object_class = associated_object_name.camelize
-  def associated_object_name  = name.demodulize
+  def associated_object_path  = associated_object_class.downcase.underscore
+  def associated_object_class = name.demodulize
 end
